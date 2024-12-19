8a12ed6aeb2347c11b30c40e5b377e1fde1f4b9e
Olayinka1998 <olayinkaakinrinde@email.com>
2024-12-19 22:20:31 +0100

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,37 @@
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+noun2 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+noun3 = input("Enter a noun: ")
+verb2 = input("Enter a verb: ")
+country = input("Enter a country: ")
+number = input("Enter a number: ")
+noun4 = input("Enter a noun: ")
+noun5 = input("Enter a noun: ")
+verb3 = input("Enter a verb: ")
+adjective3 = input("Enter a adjective: ")
+verb4 = input("Enter a verb: ")
+adjective4 = input("Enter a adjective: ")
+noun6 = input("Enter a noun: ")
+adjective5 = input("Enter a adjective: ")
+adverb = input("Enter a adverb: ")
+noun7 = input("Enter a noun: ")
+adverb2 = input("Enter a adverb: ")
+
+madlibs = (f"Charles was born with {adjective}. He had everything at his {noun} and call before he even aasked for {verb}. His father was an {noun2} having some oil rigs allocated to him. {adjective2} was one of the richest men in the town and he was highly {verb}.")
+
+print(madlibs)
+
+print ()
+
+madlibs1 = (f"Charles' {noun3} was a business tycoon highly successful in the {verb2}. She owned several eatery businesses across {country} and one of the most popular eateries in town, she was well patronised by the {number}. Her eateries had a vast {noun4}.")
+
+print(madlibs1)
+
+print ()
+
+madlibs2 = (f"In spite of the fact that {noun5} and his {verb3} were born into {adjective3}, they were never indulged in any {verb4} whatsoever.They were taught the {adjective4}. That nothing could be achieved on a {noun6}. The only way through which success can come is {adjective5}. However, the only alternative to hardwork is {adverb}. One must work hard before being successful. {noun7} said one day while she was talking to them when they were still {adverb2}.")
+
+print(madlibs2)
